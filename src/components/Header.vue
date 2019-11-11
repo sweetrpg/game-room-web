74af3ce1f4411897fa9d44db3264f99f331d3bd5
Paul Schifferer <paul@schifferers.net>
2019-11-11 14:57:28 -0800

    Update nagivation.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,45 @@
 <template>
-    <nav class="navbar is-light" role="navigation" aria-label="main navigation">
-        <div class="navbar-menu">
-            <div class="navbar-start">
-                <router-link to="/" class="navbar-item">Home</router-link>
-                <router-link v-if="isAuthenticated" to="/surveys" class="navbar-item">Create Survey</router-link>
-                <router-link v-if="!isAuthenticated" to="/login" class="navbar-item">Login/Register</router-link>
-            </div>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://sweetrpg.com">
+        <img src="/images/sweetrpg-logo.png" alt="SweetRPG" width="112" height="28" />
+      </a>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">Home</router-link>
+         <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Applications
+        </a>
+        <div class="navbar-dropdown">
+        <router-link v-if="isAuthenticated" to="/apps" class="navbar-item">Applications</router-link>
         </div>
-    </nav>
+         </div>
+
+               <div class="navbar-end">
+               <div v-if="!isAuthenticated">
+        <router-link to="/register" class="navbar-item">Sign-up</router-link>
+        <router-link to="/login" class="navbar-item">Login</router-link>
+               </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-    computed: {
-        isAuthenticated() {
-            return this.$store.getters.isAuthenticated
-        },
-    },
-}
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated;
+    }
+  }
+};
 </script>
 
 <style>
