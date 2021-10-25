a4bb7a617cce5ccee61306bb7c198a9bf79974db
Paul Schifferer <paul@schifferers.net>
2021-10-24 20:57:54 -0700

    Checkpoint.

--- src/sweetrpg_library_web/application/blueprints/volumes/__init__.py
+++ src/sweetrpg_library_web/application/blueprints/volumes/__init__.py
@@ -14,12 +14,16 @@ blueprint = Blueprint("volumes", __name__, url_prefix="/volumes")
 
 @blueprint.route("/", methods=["GET"])
 def get_volumes():
-    api_client = g[constants.API_CLIENT_KEY]
-    context = get_context().update({'volumes':[]})
+    api_client = current_app.config[constants.API_CLIENT_KEY]
+    volumes = api_client.query() # TODO
+    context = get_context().update({'volumes': volumes})
     return render_template("volumes/index.html", **context)
 
 @blueprint.route("/<id>", methods=["GET"])
 def get_volume(id:str):
-    api_client = g[constants.API_CLIENT_KEY]
-    context = {}
+    api_client = current_app.config[constants.API_CLIENT_KEY]
+    volume = api_client.get(id)
+    if not volume:
+        raise ObjectNotFound(id)
+    context = get_context().update({'volume': volume})
     return render_template("volumes/single.html", **context)
