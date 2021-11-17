bc9e4c54e8768406b1f3bb0c39918df6ec456b2a
Paul Schifferer <paul@schifferers.net>
2021-11-17 07:54:19 -0800

    Checkpoint.

--- src/sweetrpg_library_web/application/blueprints/volumes/__init__.py
+++ src/sweetrpg_library_web/application/blueprints/volumes/__init__.py
@@ -12,6 +12,7 @@ from sweetrpg_client.exceptions import NotFound
 from jsonapi_client import Session
 import os
 from sweetrpg_library_web.application.blueprints import render_page
+import json_api_doc
 
 
 blueprint = Blueprint("volumes", __name__, url_prefix="/volumes")
@@ -27,9 +28,14 @@ def get_volumes():
     # api_client = current_app.config[constants.SWEETRPG_API_CLIENT_KEY]
     # volumes = api_client.query(VOLUME) # TODO
     try:
-        with Session(f"{os.environ[constants.LIBRARY_API_BASE_URL]}") as s:
-            volumes = s.get("volumes")
-            current_app.logger.debug("volumes: %s", volumes)
+        url = os.environ[constants.LIBRARY_API_BASE_URL]
+        current_app.logger.debug("(LIBRARY_API_BASE_URL) url: %s", url)
+        with Session(url) as s:
+            result = s.get("volumes/")
+            current_app.logger.debug("result: %s", result)
+            from sweetrpg_library_objects.api.volume.schema import VolumeAPISchema
+            schema = VolumeAPISchema()
+            volumes = list(map(lambda v: schema.load(v.json), result.resources))
             context.update({'volumes': volumes})
     except:
         current_app.logger.exception("Unable to fetch volumes!")
