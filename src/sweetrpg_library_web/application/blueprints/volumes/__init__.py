4e90eb4d91c70bdf26757bb1389020c47ee788cb
Paul Schifferer <paul@schifferers.net>
2021-11-26 09:22:42 -0800

    Update requirements. Add deploy file.

--- src/sweetrpg_library_web/application/blueprints/volumes/__init__.py
+++ src/sweetrpg_library_web/application/blueprints/volumes/__init__.py
@@ -9,10 +9,8 @@ from sweetrpg_library_web.application import constants
 from sweetrpg_web_core.helpers.context import get_context
 from sweetrpg_client.types import VOLUME
 from sweetrpg_client.exceptions import NotFound
-from jsonapi_client import Session
 import os
 from sweetrpg_library_web.application.blueprints import render_page
-import json_api_doc
 
 
 blueprint = Blueprint("volumes", __name__, url_prefix="/volumes")
@@ -25,18 +23,12 @@ def get_volumes():
     """
     context = get_context()
     current_app.logger.debug("context: %s", context)
-    # api_client = current_app.config[constants.SWEETRPG_API_CLIENT_KEY]
-    # volumes = api_client.query(VOLUME) # TODO
+    api_client = current_app.config[constants.SWEETRPG_API_CLIENT_KEY]
     try:
-        url = os.environ[constants.LIBRARY_API_BASE_URL]
-        current_app.logger.debug("(LIBRARY_API_BASE_URL) url: %s", url)
-        with Session(url) as s:
-            result = s.get("volumes/")
-            current_app.logger.debug("result: %s", result)
-            from sweetrpg_library_objects.api.volume.schema import VolumeAPISchema
-            schema = VolumeAPISchema()
-            volumes = list(map(lambda v: schema.load(v.json), result.resources))
-            context.update({'volumes': volumes})
+        # url = os.environ[constants.LIBRARY_API_BASE_URL]
+        # current_app.logger.debug("(LIBRARY_API_BASE_URL) url: %s", url)
+        volumes = api_client.query(VOLUME)
+        context.update({'volumes': volumes})
     except:
         current_app.logger.exception("Unable to fetch volumes!")
         flash('Unable to fetch volumes!')
@@ -49,9 +41,14 @@ def get_volume(id:str):
     """Get a specific volume.
 
     """
+    context = get_context()
+    current_app.logger.debug("context: %s", context)
     api_client = current_app.config[constants.SWEETRPG_API_CLIENT_KEY]
-    volume = api_client.get(VOLUME, id)
-    if not volume:
-        raise NotFound(id)
-    context = get_context().update({'volume': volume})
+    try:
+        volume = api_client.get(VOLUME, id)
+        context.update({'volume': volume})
+    except:
+        current_app.logger.exception(f"Unable to fetch volume {id}!")
+        flash(f'Unable to fetch volume {id}!')
+
     return render_page("apps/library/volumes/single.html", context=context)
